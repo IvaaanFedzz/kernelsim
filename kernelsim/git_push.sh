b0f17f2f83ff510de530079926537da34a957a79
Ivan Fernandez <zyc8sjbsmr@privaterelay.appleid.com>
2025-11-05 22:36:13 +0100

    Update git_push.sh

--- kernelsim/git_push.sh
+++ kernelsim/git_push.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Script helper to init a local repo and push to your GitHub remote.
-# Usage:
-#   1) Edit REMOTE_URL below with your repository URL (or pass as first arg).
-#   2) Run: ./git_push.sh
 REMOTE_URL=$1
 if [ -z "$REMOTE_URL" ]; then
   echo "Usage: $0 <git-remote-url>"
